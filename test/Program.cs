edbdfd1b82be7ffe831841abfdc2eb1677d46c7c
Андрей <anderkoval3@gmail.com>
2019-10-04 20:41:54 +0300

    some test dir to learn inheritance

---
+++ test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace test
+{
+
+    class A {
+        public virtual void M() {
+            Console.WriteLine("A.M()");
+        }
+    }
+
+    class B : A {
+        public override void M() {
+            Console.WriteLine("B.M()");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new B();
+            a.M();
+        }
+    }
+}
