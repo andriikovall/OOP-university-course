2276a50a46bd272015d2e869d24aa60fb57877df
ziovio <anderkoval3@gmail.com>
2020-03-12 14:45:59 +0200

    input values changed

--- term2_Design-patterns/labs/lab4/src/task1/main.ts
+++ term2_Design-patterns/labs/lab4/src/task1/main.ts
@@ -7,5 +7,5 @@ const salesService = new SaleManagementService();
 
 const payment1 = salesService.getPayement(new Client('andrii', 'koval', new Date('03/08/2020'), new Date('2017/07/25')), 100);
 console.log('payment1:', payment1);
-const payment2 = salesService.getPayement(new Client('andrii', 'koval', new Date('03/01/2020'), new Date('2020/07/25')), 111);
+const payment2 = salesService.getPayement(new Client('andrii', 'koval', new Date('03/01/2020'), new Date('2020/07/25')), 100);
 console.log('payment2:', payment2);
